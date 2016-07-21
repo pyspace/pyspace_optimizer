cb6439548a47a9e0b7a319ec04783997dcb541c4
Torben Hansing <hansa@tzi.de>
2016-07-21 12:43:51 +0200

    Reverted optimization

--- pySPACEOptimizer/core/optimizer_pool.py
+++ pySPACEOptimizer/core/optimizer_pool.py
@@ -24,7 +24,8 @@ class OptimizerPool(Pool):
     def __init__(self, processes=None, initializer=None, initargs=None):
         if initargs is None:
             initargs = ()
-        super(OptimizerPool, self).__init__(processes=processes, initializer=initializer, initargs=initargs)
+        super(OptimizerPool, self).__init__(processes=processes, initializer=initializer, initargs=initargs,
+            maxtasksperchild=10)
 
     #        signal.signal(signal.SIGTERM, self._handle_signal)
     #        signal.signal(signal.SIGINT, self._handle_signal)
